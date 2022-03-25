a73965d0d99f2c551f22107048a1c332a42d7a37
rayane-barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2022-03-25 18:45:08 -0300

    funcoes atualizado

--- PraticaCompetencia_IV/ex1.cpp
+++ PraticaCompetencia_IV/ex1.cpp
@@ -34,10 +34,30 @@ void separaLinha()
 void registro()
 {
 	
-	int RA[50], i, alunoRegistrado;
+	int RA[50], i, count=0;
+	int qtdAlunos = 50;
+	char nome[50];
+	
+		for(i=1; i<=qtdAlunos; i++){
+			printf("Informe o RA: ");
+			scanf("%d", &RA[i]);
+			count ++;
+			if(RA[i]<=0)
+			
+				break;
+				
+			else {
+				printf("Infome o Nome:");
+				scanf("%s", &nome[i]);
+			}
+		}
+		
+		/*for(i=1; i<=count; i++){
+		printf("RA aluno %i: %d - ", i, RA[i]);
+		printf("Nome aluno: %c \n",  nome[i]);
 	
 		
-		
+		}*/		
 	
 }
 
