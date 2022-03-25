50f1aef627cdf5e20d06e4aad246fcccffa25dd4
rayane-barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2022-03-25 18:08:43 -0300

    funcoes

--- PraticaCompetencia_IV/ex1.cpp
+++ PraticaCompetencia_IV/ex1.cpp
@@ -9,9 +9,12 @@ void gabarito()
 	
 	char i, resposta[10];
 	
+	
+	
 	for(i=1; i<=10; i++){
 		printf("Insira a resposta da %.ia questao: " , i);
 		scanf("%s" , &resposta[i]);
+		
 			
 			
 	}
@@ -22,15 +25,54 @@ void gabarito()
 	}*/
 	
 }
-	
-	
-	
-	
-	
-			
+
+void separaLinha()
+{
+	printf("\n\n\n");
 }
 
+void registro()
+{
+	
+	int RA[50], i, alunoRegistrado;
+	
+		
+		
+	
+}
+
+
+//Funcao Questionario
+void questionario()
+{
+	char respostasQuestionario[10], i;
+		for(i=1; i<=10; i++){
+		printf("Insira a resposta da %.ia questao: " , i);
+		scanf("%s" , &respostasQuestionario[i]);
+	
+}
+
+
+
+		/*for(i=1; i<=10; i++){
+		printf("Resposta da %.ia questao: %c \n", i, respostasQuestionario[i]);
+	}*/
+}
+			
+
+
 int main(){
+	
+	printf("Informe abaixo as respostas para criar o gabarito \n\n");
+	
 	gabarito();
+	
+	separaLinha();
+	
+	printf("Registre abaixo as respostas do questionario: \n\n");
+	
+	questionario();
+	
+	separaLinha();
 	
 }
