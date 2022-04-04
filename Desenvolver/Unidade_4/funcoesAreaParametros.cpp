49dc3a65f4d244f510e79e107ac724db2603125a
rayane-barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2022-04-03 22:57:02 -0300

    AreaTriangulo

--- Desenvolver/Unidade_4/funcoesAreaParametros.cpp
+++ Desenvolver/Unidade_4/funcoesAreaParametros.cpp
@@ -12,18 +12,50 @@ float AreaRetangulo(float b, float h)
 }
 
 
+//Exercicio 2 - função para calcular a area do triangulo 
+float AreaTriangulo(float b, float h)
+{
+	float areaTriangulo;
+	
+	areaTriangulo = (b*h)/2;
+	return areaTriangulo;
+}
+
+
 
 
 int main (){
 	float b, h, resultado;
+	int escolhaFigura =0;
 	
-	printf("Infome a base: ");
+	printf("Informe a base:");
 	scanf("%f", &b);
-	printf("Informe a altura: ");
+	printf("Informe a altura:");
 	scanf("%f", &h);
 	
-	resultado = AreaRetangulo(b,h);
-	printf("Area do retangulo: %.2f", resultado);
+	printf("\n\nEscolha a figura e o programa ira calcular sua área \n\n");
+	printf("1. Retangulo\n");
+	printf("2. Triangulo\n");
+	scanf("%d", &escolhaFigura);
 	
-	
+	switch(escolhaFigura){
+		case 1:
+			 resultado = AreaRetangulo(b,h);
+			 printf("\nA area do retangulo e: %f", resultado);
+			 break;
+		
+		case 2:
+			 resultado = AreaTriangulo(b,h);
+			 printf("\nA area do triangulo e: %f", resultado);
+			 break;
+			
+	}
 }
+	
+	
+	
+	
+	
+	
+	
+
